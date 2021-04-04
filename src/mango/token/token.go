bbf86f6d71ea7e43d371e9c8d39663984ca60094
Claudio Romero <romerolrocha@gmail.com>
2021-04-04 00:15:23 -0300

    Improve lexer

--- src/mango/token/token.go
+++ src/mango/token/token.go
@@ -24,7 +24,7 @@ const (
 	SEMICOLON = ";"
 
 	LPAREN = "("
-	RPAREM = ")"
+	RPAREN = ")"
 	LBRACE = "{"
 	RBRACE = "}"
 
@@ -32,3 +32,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
