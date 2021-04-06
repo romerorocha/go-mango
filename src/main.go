f677a38eaac9a81faeb578715e9753210db61733
Claudio Romero <romerolrocha@gmail.com>
2021-04-05 22:55:58 -0300

    Remove 'mango' dir

--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Hello %s! This is the Mango programming language\n", user.Username)
+	fmt.Printf("Hello %s! This is the mango programming language\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
