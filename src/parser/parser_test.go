f02636c842da4b4a1dde076d4817ab53e458780a
Claudio Romero <romerolrocha@gmail.com>
2021-04-10 11:50:04 -0300

    Add 'return statements' parser

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -23,7 +23,7 @@ func TestLetStatements(t *testing.T) {
 		t.Fatalf("ParseProgram() returned nil")
 	}
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 statements. got %d", len(program.Statements))
+		t.Fatalf("program.Statements does not contain 3 statements, got %d", len(program.Statements))
 	}
 
 	tests := []struct {
@@ -44,24 +44,54 @@ func TestLetStatements(t *testing.T) {
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
-		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		t.Errorf("s.TokenLiteral not 'let', got=%q", s.TokenLiteral())
 		return false
 	}
 
 	letStmt, ok := s.(*ast.LetStatement)
 	if !ok {
-		t.Errorf("s not *ast.LetStatement. got%T", s)
+		t.Errorf("s not *ast.LetStatement, got%T", s)
 	}
 
 	if letStmt.Name.Value != name {
-		t.Errorf("letSmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		t.Errorf("letSmt.Name.Value not '%s', got=%s", name, letStmt.Name.Value)
 	}
 
 	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		t.Errorf("s.Name not '%s', got=%s", name, letStmt.Name)
 	}
 
 	return true
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements, got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
+	}
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
